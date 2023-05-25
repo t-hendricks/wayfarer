103ffc3a19a9a89f5900f720a30ea8e757890df3
kimnguyen <knnguyen2410@gmail.com>
2023-05-25 10:46:48 -0500

    Created homepage routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeroComponent } from './hero/hero.component';
 import { TopicComponent } from './topic/topic.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HeroComponent,
-    TopicComponent
+    TopicComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomepageComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
