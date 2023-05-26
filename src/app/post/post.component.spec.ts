51afda2fa7022ba2f8c5c6d8162ed2c9913d263f
kimnguyen <knnguyen2410@gmail.com>
2023-05-26 11:19:28 -0500

    Resolve failed tests

--- src/app/post/post.component.spec.ts
+++ src/app/post/post.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostComponent } from './post.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('PostComponent', () => {
   let component: PostComponent;
@@ -8,7 +9,8 @@ describe('PostComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PostComponent]
+      declarations: [PostComponent],
+      imports: [RouterTestingModule]
     });
     fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
