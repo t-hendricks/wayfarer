70a0a00fccd4592b695a83b36b29129b6f9c6b63
kimnguyen <knnguyen2410@gmail.com>
2023-05-27 04:33:38 -0500

    Create test to validate that a post's title must be between 1 and 200 characters

--- src/app/post/post.component.spec.ts
+++ src/app/post/post.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostComponent } from './post.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { posts, Posts } from '../data/postsdata';
 
 describe('PostComponent', () => {
   let component: PostComponent;
@@ -19,5 +20,14 @@ describe('PostComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('title', () => {
+    it('should have a length between 1 and 200 characters', () => {
+      for (let post of posts) {
+        let validTitle = component.validatePostTitle(post.title);
+        expect(validTitle).toBeTrue();
+      }
+    })
   });
 });
