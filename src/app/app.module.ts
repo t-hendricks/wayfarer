15511f530c36abe09a6bdc402c69d69a13a584be
kimnguyen <knnguyen2410@gmail.com>
2023-05-29 11:33:28 -0500

    Call Open Weather API for each city to display the current weather on each city's page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -22,7 +23,8 @@ import { FooterComponent } from './landingpage/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     LandingpageModule,
-    NavBarModule
+    NavBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
