3be7e673662aaa6b842e4d85eeca6582e9c1217f
kimnguyen <knnguyen2410@gmail.com>
2023-05-29 13:46:43 -0500

    Imported router testing module, all tests pass

--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavBarComponent } from './nav-bar.component';
 import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
@@ -10,7 +11,7 @@ describe('NavBarComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [NavBarComponent],
-      imports: [FormsModule]
+      imports: [FormsModule, RouterTestingModule]
     });
     fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
