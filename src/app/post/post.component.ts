4d5f97be220640e2a85cedc84af74a9618d0e11d
kimnguyen <knnguyen2410@gmail.com>
2023-05-29 19:25:15 -0500

    Add docstrings to post.component.ts methods

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -17,7 +17,11 @@ export class PostComponent implements OnInit {
 
   constructor(private route: ActivatedRoute){}
 
-  ngOnInit() {
+  /**
+   * The ngOnInit method initialized the post component and grabs the post route's parameter in the URL.
+   * The postId parameter that is grabbed is used to find the corresponding post from the postsdata.ts file through the post's id.
+   */
+  ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const paramPostId: string = params.get('postId') || '';
       this.URLposts = cities.flatMap(city => city.posts);
@@ -25,6 +29,11 @@ export class PostComponent implements OnInit {
     });    
   }
 
+  /**
+   * The validatePostTitle method is used to validate that the post's title is between 1 and 200 characters in length.
+   * @param postTitle is the post's title to be validated.
+   * @returns false if the post's title is not between 1 and 200 characters in length, and true otherwise.
+   */
   validatePostTitle(postTitle: string): boolean {
     for (let post of this.posts) {
       if (!(post.title.length >= 1 && post.title.length <= 200)) {
@@ -34,6 +43,11 @@ export class PostComponent implements OnInit {
     return true;
   }
 
+  /**
+   * The validatePostContent method is used to validate that the post's content is not empty.
+   * @param postContent is the post's content to be validated.
+   * @returns false if the post's content is empty (with a length of 0 characters), and true otherwise.
+   */
   validatePostContent(postContent: string): boolean {
     for (let post of this.posts) {
       if (post.content.length === 0) {
