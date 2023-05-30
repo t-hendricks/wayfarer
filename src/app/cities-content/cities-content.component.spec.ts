62b50db60a5d2a0dc4754eedf9f4679e6b34eabe
kimnguyen <knnguyen2410@gmail.com>
2023-05-29 19:56:38 -0500

    Rearrange imports for cities-content.component.spec.ts

--- src/app/cities-content/cities-content.component.spec.ts
+++ src/app/cities-content/cities-content.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { CitiesContentComponent } from './cities-content.component';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { CitiesContentComponent } from './cities-content.component';
 import { CitiesNavComponent } from '../cities-nav/cities-nav.component';
 
 describe('CitiesContentComponent', () => {
