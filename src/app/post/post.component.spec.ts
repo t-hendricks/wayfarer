42ed12d99aa71f021e029f97328003064e4e88b4
kimnguyen <knnguyen2410@gmail.com>
2023-05-29 19:27:20 -0500

    Add docstrings to the post title and content tests

--- src/app/post/post.component.spec.ts
+++ src/app/post/post.component.spec.ts
@@ -22,6 +22,9 @@ describe('PostComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  /**
+   * Tests if the post's title is between 1 and 200 characters using the validatePostTitle method in the post.component.ts file.
+   */
   describe('title', () => {
     it('should have a length between 1 and 200 characters', () => {
       for (let post of posts) {
@@ -31,6 +34,9 @@ describe('PostComponent', () => {
     })
   });
 
+  /**
+   * Tests if the post's content is not empty using the validatePostContent method in the post.component.ts file.
+   */
   describe('content', () => {
     it('should not be empty', () => {
       for (let post of posts) {
