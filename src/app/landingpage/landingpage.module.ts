7d293549801e63617c52a5dcb6a9f7211710d701
kimnguyen <knnguyen2410@gmail.com>
2023-05-29 19:38:30 -0500

    Rearrange imports in landingpage.module.ts

--- src/app/landingpage/landingpage.module.ts
+++ src/app/landingpage/landingpage.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app.routing.module';
+
 import { HeroComponent } from './hero/hero.component';
 import { TopicComponent } from './topic/topic.component';
 import { LandingpageComponent } from './landingpage.component';
 import { CitySquareComponent } from './city-square/city-square.component';
-import { AppRoutingModule } from '../app.routing.module';
 
 
 @NgModule({
